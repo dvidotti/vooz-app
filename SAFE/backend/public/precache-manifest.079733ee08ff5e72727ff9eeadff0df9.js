383b6b61ed3dc0a65109229c79eed0edddd43427
dvidotti <danilovidotti@gmail>
2019-08-19 08:29:01 -0300

    insert bundle for deploy

---
+++ SAFE/backend/public/precache-manifest.079733ee08ff5e72727ff9eeadff0df9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6966f4a2cca5c1f296ae63e7907cf40e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1adda523772c24ae57b7",
+    "url": "/static/css/main.b5b5b780.chunk.css"
+  },
+  {
+    "revision": "e4b6bc8be6828b0270fe",
+    "url": "/static/js/2.a5d62e22.chunk.js"
+  },
+  {
+    "revision": "1adda523772c24ae57b7",
+    "url": "/static/js/main.c3422a3c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
